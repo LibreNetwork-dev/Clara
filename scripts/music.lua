700724b8e041626dbe0144c9d1d998faa28f9f61
Shrey719 <shrey150436@gmail.com>
2025-06-26 16:38:58 -0400

    things

--- scripts/music.lua
+++ scripts/music.lua
@@ -11,9 +11,17 @@ function play(query)
     handle:close()
 
     local cachePath = "./cache/music/" .. vidId .. ".mp4"
-
+    os.execute("pkill mpv")
+    while os.execute("pgrep mpv > /dev/null") == 0 do end
     if fileExists(cachePath) then
-        os.execute("mpv --no-video '" .. cachePath .. "'") 
+        local res = os.execute("mpv --no-video '" .. cachePath .. "'") 
+        if (res ~= 0) then
+            -- re dl if the file is malformed
+            os.execute(string.format(
+                "%s -f bestaudio -o - https://youtube.com/watch?v=%s | tee '%s' | mpv --no-video -",
+                dlpPath, vidId, cachePath
+            ))
+        end
     else --cache & play at the same time so its fasttttt
         os.execute(string.format(
             "%s -f bestaudio -o - https://youtube.com/watch?v=%s | tee '%s' | mpv --no-video -",
